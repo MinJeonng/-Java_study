1f2b8b7ed08f131b1df5fe80dd2e2201824e522e
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-04-18 20:49:51 +0900

    메소드 오버로딩 실

--- JavaStudy/src/Java_0418/Prac1.java
+++ JavaStudy/src/Java_0418/Prac1.java
@@ -1,19 +1,24 @@
 package Java_0418;
-import java.util.Scanner;
 
-//아직하는
 public class Prac1 {
-	public double circle(double radius){
+	public double circle(int radius){
 		return radius * Math.PI * radius;
 	}
-
+	public int square(int vertical, int horizontal) {
+		return vertical * horizontal;
+	}
+	public int triangle(int height, int base) {
+		return height * base * 1/2;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("나이를 입력하세요 ");
-		Scanner scan = new Scanner(System.in);
-		int age = scan.nextInt();
+		Prac1 calc = new Prac1();
 		
+		System.out.printf("반지름이 %d인 원의 넓이 : %f", 5, calc.circle(5)).println();
+		System.out.printf("가로 %d,세로 %d인 직사각형의 넓이 : %d", 4,7,calc.square(4, 7)).println();
+		System.out.printf("밑변 %d,높이 %d인 삼각형의 넓이 : %d", 6,3,calc.triangle(6, 3));
 		
-		scan.close();
+
 	}
 
 }
